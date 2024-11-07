b2691ecc43a69d22d2efa7d9f27d04805599d431
Sumathi Dhanasekaran <L00187746@atu.ie>
2024-11-07 21:26:59 +0000

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,11 @@
+def hello_world():
+    """
+    Prints 'Hello, World!' to the console.
+
+    This function serves as a basic example of a Python function with a docstring.
+    """
+    print("Hello, World!")
+
+
+# Call the function
+hello_world()
